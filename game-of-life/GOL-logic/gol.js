c841f49e78e3a6bc0b06d2f51b2bcccfeecef05e
SMoore3773 <Smooresculpture@gmail.com>
2020-08-26 21:36:41 -0400

    cra reset

---
+++ game-of-life/GOL-logic/gol.js
@@ -0,0 +1,12 @@
+
+
+grid = new Array(col);
+for (let i = 0; i< col; i++) {
+    grid[i] = new Array(row);
+}
+
+tempGrid = new Array(col);
+for (let i = 0; i<col; i++) {
+    tempGrid[i] = new Array(row);
+}
+
