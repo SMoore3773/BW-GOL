0da91a98de3e356ca5456cc89206dd62ca48327f
SMoore3773 <Smooresculpture@gmail.com>
2020-10-23 02:54:59 -0400

    .

--- game-of-life/GOL-logic/gol.js
+++ game-of-life/GOL-logic/gol.js
@@ -1,12 +1,42 @@
 
+let toggleRun;
+let reset;
+let iterate;
 
-grid = new Array(col);
-for (let i = 0; i< col; i++) {
-    grid[i] = new Array(row);
+const game = (props) =>{
+
+    return(p5) =>{
+
+        const iteration = props.iteration;
+        const pixelWidth = props.puxelWidth;
+        const cells = props.cells;
+        const cellSize = pixelWidth/cells;
+
+        let generation = 0;
+        let running = false;
+        const births = [];
+        const deaths = [];
+        let grid;
+        let gridBuffer;
+        
+        p5.setup = () =>{
+            const canvas = p5.createCanvas()
+            
+        }
+        function makeGrid() {
+            
+            grid = new Array(col);
+            for (let i = 0; i< col; i++) {
+                grid[i] = new Array(row);
+            }
+        
+            tempGrid = new Array(col);
+            for (let i = 0; i<col; i++) {
+                tempGrid[i] = new Array(row);
+            }   
+        }
+
+    }
 }
 
-tempGrid = new Array(col);
-for (let i = 0; i<col; i++) {
-    tempGrid[i] = new Array(row);
-}
 
